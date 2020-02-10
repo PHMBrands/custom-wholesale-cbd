f50bf036ae6d32180c7572a7e12c62d65f4eb5f3
Mark R. Collins <markcollins.md@gmail.com>
2020-02-09 17:46:44 -0700

    Functionality good pause for poise spot

--- src/components/TinctureChoices/TinctureChoices.js
+++ src/components/TinctureChoices/TinctureChoices.js
@@ -58,19 +58,19 @@ class TinctureChoices extends Component {
             <h4>Please select your flavor:</h4>
             <form>
               <input type="radio" id="guava-lime" name="flavor" value="Guava Lime" onChange={this.handleChangeNoButton} />
-                <label for="guava-lime">Guava Lime</label>
+                <label htmlFor="guava-lime">Guava Lime</label>
               <input type="radio" id="mocha" name="flavor" value="Mocha" onChange={this.handleChangeNoButton} />
-                <label for="mocha">Mocha</label>
+                <label htmlFor="mocha">Mocha</label>
               <input type="radio" id="mojito" name="flavor" value="Mojito" onChange={this.handleChangeNoButton} />
-                <label for="mojito">Mojito</label>
+                <label htmlFor="mojito">Mojito</label>
               <input type="radio" id="orange-creamsicle" name="flavor" value="Orange Creamsicle" onChange={this.handleChangeNoButton} />
-                <label for="orange-creamsicle">Orange Creamsicle</label>
+                <label htmlFor="orange-creamsicle">Orange Creamsicle</label>
               <input type="radio" id="peppermint" name="flavor" value="Peppermint" onChange={this.handleChangeNoButton} />
-                <label for="peppermint">Peppermint</label>
+                <label htmlFor="peppermint">Peppermint</label>
               <input type="radio" id="mint-chocolate" name="flavor" value="Mint Chocolate" onChange={this.handleChangeNoButton} />
-                <label for="mint-chocolate">Mint Chocolate</label>
+                <label htmlFor="mint-chocolate">Mint Chocolate</label>
               <input type="radio" id="unflavored" name="flavor" value="unflavored" onChange={this.handleChangeNoButton} />
-                <label for="unflavored">unflavored</label>
+                <label htmlFor="unflavored">unflavored</label>
             </form>
           </article>
       case 'carrier':
@@ -78,9 +78,9 @@ class TinctureChoices extends Component {
             <h4>Please select your carrier oil:</h4>
             <form>
               <input type="radio" id="wheat-germ-oil" name="carrier" value="Wheat Germ Oil" onChange={this.handleChangeNoButton} />
-                <label for="wheat-germ-oil">Wheat Germ Oil</label>
+                <label htmlFor="wheat-germ-oil">Wheat Germ Oil</label>
               <input type="radio" id="mct-oil" name="carrier" value="MCT Oil" onChange={this.handleChangeNoButton} />
-                <label for="mct-oil">MCT Oil</label>
+                <label htmlFor="mct-oil">MCT Oil</label>
             </form>
           </article>
       case 'spectrum':
@@ -88,9 +88,9 @@ class TinctureChoices extends Component {
           <h4>Please select your spectrum:</h4>
           <form>
             <input type="radio" id="full-spectrum" name="spectrum" value="Full Spectrum" onChange={this.handleChangeNoButton} />
-              <label for="full-spectrum">Full Spectrum</label>
+              <label htmlFor="full-spectrum">Full Spectrum</label>
             <input type="radio" id="broad-spectrum" name="spectrum" value="Broad Spectrum" onChange={this.handleChangeNoButton} />
-              <label for="broad-spectrum">Broad Spectrum</label>
+              <label htmlFor="broad-spectrum">Broad Spectrum</label>
           </form>
         </article>
       case 'potency':
@@ -98,13 +98,13 @@ class TinctureChoices extends Component {
             <h4>Please select your potency per serving:</h4>
             <form>
               <input type="radio" id="10mg" name="potency" value="10 mg" onChange={this.handleChangeNoButton} />
-                <label for="10mg">10 mg</label>
+                <label htmlFor="10mg">10 mg</label>
               <input type="radio" id="20mg" name="potency" value="20 mg" onChange={this.handleChangeNoButton} />
-                <label for="20mg">20 mg</label>
+                <label htmlFor="20mg">20 mg</label>
               <input type="radio" id="30mg" name="potency" value="30 mg" onChange={this.handleChangeNoButton} />
-                <label for="30mg">30 mg</label>
+                <label htmlFor="30mg">30 mg</label>
               <input type="radio" id="50mg" name="potency" value="50 mg" onChange={this.handleChangeNoButton} />
-                <label for="50mg">50 mg</label>
+                <label htmlFor="50mg">50 mg</label>
             </form>
           </article>
       case 'logo':
